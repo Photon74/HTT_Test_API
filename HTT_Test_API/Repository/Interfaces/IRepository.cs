c786d557647efeb7590b28768d0eea42f6baa1ff
Alexandr Kolodinsky <Photon.74@gmail.com>
2023-05-07 17:17:52 +0300

    fix

--- HTT_Test_API/Repository/Interfaces/IRepository.cs
+++ HTT_Test_API/Repository/Interfaces/IRepository.cs
@@ -3,13 +3,5 @@
     public interface IRepository<TEntity, TId>
     {
         IList<TEntity> GetAll();
-
-        //TEntity GetById(TId id);
-
-        //TId Create(TEntity data);
-
-        //int Update(TEntity data);
-
-        //int Delete(TId id);
     }
 }
